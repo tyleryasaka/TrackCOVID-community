dc8da68e96611b01ae6b6aee485cb5ae5eb25c86
Tyler Yasaka <contact@tyleryasaka.me>
2020-05-11 02:45:49 -0700

    Include admin build files

---
+++ admin/build/precache-manifest.667dab8980c91a8dbc014e6ff4766841.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "464cfe82fecfa41d9abc9eb703d15286",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "042831811d4a39d4b86a",
+    "url": "/admin/static/css/main.d3df0567.chunk.css"
+  },
+  {
+    "revision": "f1c8c29ba29307869e33",
+    "url": "/admin/static/js/2.5ec62e4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/admin/static/js/2.5ec62e4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "042831811d4a39d4b86a",
+    "url": "/admin/static/js/main.68502ce6.chunk.js"
+  },
+  {
+    "revision": "91977334a70bc0b7aacf",
+    "url": "/admin/static/js/runtime-main.1ec06687.js"
+  }
+]);
