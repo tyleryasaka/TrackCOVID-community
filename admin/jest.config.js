cf39d1389ee1304c864217d634b1d815d7e88158
Tyler Yasaka <contact@tyleryasaka.me>
2020-05-13 21:27:15 -0700

    Test api requests in admin

--- admin/jest.config.js
+++ admin/jest.config.js
@@ -1,7 +1,3 @@
-process.env.REACT_APP_NAME = 'TrackCOVID'
-process.env.REACT_APP_WEB_APP_URL = 'https://demo.trackcovid.net'
-process.env.REACT_APP_REGISTRATION_URL = 'http://my-form.my-org'
-
 module.exports = {
   // Automatically clear mock calls and instances between every test
   clearMocks: true,
