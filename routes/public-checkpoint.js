6bfbe35e40c5ae373865d6129d486d67f453d829
Tyler Yasaka <contact@tyleryasaka.me>
2020-05-14 02:30:15 -0700

    Cleaning up

--- routes/public-checkpoint.js
+++ routes/public-checkpoint.js
@@ -13,10 +13,10 @@ publicCheckpointApiRouter.get('/', async (req, res) => {
   const checkpointKey = sha256(String(Math.random())).substring(0, checkpointKeyLength)
   const appLink = process.env.APP_URL
   const checkpointLink = `${appLink}/?checkpoint=${checkpointKey}`
-  const checkpointQrCodeUrl = await QRCode.toDataURL(checkpointLink, { margin: 0 })
+  const checkpointQrCodeUrl = await QRCode.toDataURL(checkpointLink, { margin: 0, scale: 20 })
   const checkpointQrCodeImg = Buffer.from(checkpointQrCodeUrl.replace('data:image/png;base64,', ''), 'base64')
   const websiteLink = process.env.ABOUT_URL
-  const websiteQRCodeUrl = await QRCode.toDataURL(websiteLink, { margin: 0 })
+  const websiteQRCodeUrl = await QRCode.toDataURL(websiteLink, { margin: 0, scale: 4 })
   const websiteQrCodeImg = Buffer.from(websiteQRCodeUrl.replace('data:image/png;base64,', ''), 'base64')
   doc.image('./public-checkpoint/track-covid.png', 0, 0, { width: 600 })
   doc.image(checkpointQrCodeImg, 55, 325, { width: 300 })
