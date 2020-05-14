73bae273d2caecebecc183f65a6dad3eb1bbb3ff
Tyler Yasaka <contact@tyleryasaka.me>
2020-05-13 22:57:23 -0700

    Fix test setup

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,10 +1,3 @@
-process.env.REACT_APP_NAME = 'TrackCOVID'
-process.env.REACT_APP_THEME = 'dark'
-process.env.REACT_APP_ABOUT_URL = 'https://trackcovid.net'
-process.env.REACT_APP_ESTIMATED_DX_DELAY_DAYS = 7
-process.env.REACT_APP_CONTACT_WINDOW_HOURS_BEFORE = 1
-process.env.REACT_APP_CONTACT_WINDOW_HOURS_AFTER = 2
-
 module.exports = {
   // Automatically clear mock calls and instances between every test
   clearMocks: true,
