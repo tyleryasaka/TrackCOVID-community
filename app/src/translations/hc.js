43d9e00e77132fee00d4b447b8453cb7a939bd0a
John Dovale <66154260+jlrdovale@users.noreply.github.com>
2020-06-06 22:25:52 +1200

    Create hc.js

---
+++ app/src/translations/hc.js
@@ -0,0 +1,91 @@
+
+
+const appName = process.env.REACT_APP_NAME
+
+
+
+module.exports = {
+
+  'translation': {
+
+    'menuAboutButton': 'Sou',
+
+    'menuCheckpointButton': 'Tcheke pwen',
+
+    'menuAdminButton': 'Admin',
+
+    'checkpointsTab': 'Tcheke pwens',
+
+    'statusTab': 'Estati',
+
+    'settingsTab': 'Anviwonman',
+
+    'welcomeMessage': `Byenveni nan ${appName}. Pou patisipe nan efo a yo swiv gaye nan SARS-COV-2 viris la, tanpri analysis yon tipos chak fwa ou kominike avek lot moun nan yon fason ki ta ka transmet viris la.`,
+
+    'hostCheckpointButton': 'Oganize yon Tipos',
+
+    'joinCheckpointButton': 'Analysis yon Tipos',
+
+    'hostingCheckpointMessage': 'Ou kounye a oganize yon pwen chek. Gen lot ki ka rantre nan Kod QR ki anba a.',
+
+    'endCheckpointButton': 'Fen chek pwen',
+
+    'printCheckpointButton': 'Enprime',
+
+    'checkpointCreatedMessage': 'Tcheke pwen kreye',
+
+    'noCameraPermissionMessage': "Sa a app pa gen pemisyon jwenn akse nan kamera aparey ou a. Olye de sa, ou ka pran yon foto nan Kod QR la.",
+
+    'takePictureButton': 'Pran yon foto',
+
+    'backButton': 'Retounen',
+
+    'joinSuccessfulMessage': 'Ou te mete pwen an chek avek sikse.',
+
+    'scanErrorMessage': 'Kod QR la pa t ' kapab li. Tanpri eseye anko.',
+
+    'statusLoadingMessage': 'Chaje estati ou...',
+
+    'statusNegativeMessage': 'Pa gen okenn chemen transmisyon nan men moun ki enfekte pou ou yo te dekouvri nan moman sa a. Sepandan, tout moun se risk ak moun ki dwe suiv direksyon otorite sante piblik la, gouvenman lokal.',
+
+    'statusPositiveMessage': 'Yon chemen transmisyon posib ki soti nan yon moun ki enfekte a ou te dekouvri. Ou ta dwe pran mezi pwekosyon pou pwoteje tet ou ak lot moun, dapre direksyon otorite sante piblik la & gouvenman lokal.',
+
+    'loadingMessage': 'mete abo...',
+
+    'standardRiskLevelMessage': 'nomal',
+
+    'elevatedRiskLevelMessage': 'elve',
+
+    'yourRiskLevelMessage': 'Nivo risk ou',
+
+    'aboutReportMessage': 'Si ou te resevwa yon tes pozitif, ou ka telechaje denye baraj ou anba a epi pataje dosye sa a ak dokte ou. Sa ap aveti moun ki ka ekspoze anba risk ogmante yo. Ou ap rete anonim.',
+
+    'reportButton': 'Rapo anonim',
+
+    'downloadHistoryButton': 'Resevwa pwen chek',
+
+    'aboutConfirmationCodeMessage': 'Ou gen yon Kod konfimasyon pou analysis? Fe enspeksyon yon Kod konfimasyon pral ede moun ki ka yo te ekspoze, pa kite yo konnen ke sa a se yon risk lejitim.',
+
+    'scanConfirmationCodeButton': 'Eskane Kod konfimasyon',
+
+    'scanWithoutConfirmationCodeButton': "Mwen pa gen Kod",
+
+    'reportConfirmationMessage': 'Sa ap notifye moun ki ka ekspoze anba risk ogmante yo. Ou ap rete anonim. Sa pa kapab ti mouton sele.',
+
+    'reportCompletedMessage': 'Te estati pozitif ou te rapote anonim. Moun ki nan risk yo pral notifye. Mesi.',
+
+    'cancelReportButton': 'bliye sa',
+
+    'confirmReportButton': 'Rapo kounye a',
+
+    'aboutUseConfirmedDiagnoses': 'Chwazi  "sevi ak selman konfime dyagnostik " pral inyore chemen transmisyon posib nan rapo ki pa konfime.',
+
+    'useConfirmedDiagnosesButton': 'Sevi ak selman konfime dyagnostik',
+
+    'elevatedRiskAlertMessage': 'Nivo risk ou wo.',
+
+    'seeStatusTabMessage': 'Gade tab la estati.'
+
+  }
+
+}
