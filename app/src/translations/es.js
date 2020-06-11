91c406d1c0f937ea487fad1a3f86781ee16a3df7
John Dovale <66154260+jlrdovale@users.noreply.github.com>
2020-06-11 12:49:57 +1200

    Add files via upload

--- app/src/translations/es.js
+++ app/src/translations/es.js
@@ -20,37 +20,37 @@ module.exports = {
 
     'settingsTab': 'Configuración',
 
-    'welcomeMessage': `Bienvenido a ${appName}. Para participar en el esfuerzo por rastrear la propagación del virus SARS-COV-2, escanee un punto de control cada vez que interactúe con otros de una manera que pueda transmitir el virus.`,
+    'welcomeMessage': `Bienvenido a ${appName}. Para participar en el esfuerzo para realizar un seguimiento de la propagación del virus SARS-COV-2, por favor escanee un punto de control cada vez que interactúe con otros de una manera que pueda transmitir el virus.`,
 
-    'hostCheckpointButton': 'Alojar un punto de control',
+    'hostCheckpointButton': 'Aloje un punto de control',
 
-    'joinCheckpointButton': 'Escanear un punto de control',
+    'joinCheckpointButton': 'Escanea un punto de control',
 
     'hostingCheckpointMessage': 'Ahora está alojando un punto de control. Otros pueden unirse usando el código QR a continuación.',
 
-    'endCheckpointButton': 'Fin del punto de control',
+    'endCheckpointButton': 'Final del punto de control',
 
-    'printCheckpointButton': 'Impresión',
+    'printCheckpointButton': 'Imprimir',
 
     'checkpointCreatedMessage': 'Punto de control creado',
 
-    'noCameraPermissionMessage': "Esta aplicación no tiene permiso para acceder a la cámara de su dispositivo. En su lugar, puede tomar una foto del código QR.",
+    'noCameraPermissionMessage': "Esta aplicación no tiene permiso para acceder la cámara de su dispositivo. En vez, puede tomar una foto del código QR.",
 
-    'takePictureButton': 'Tome una foto',
+    'takePictureButton': 'Tomar una foto',
 
-    'backButton': 'Atrás',
+    'backButton': 'Devuelta',
 
-    'joinSuccessfulMessage': 'Se ha unido al punto de control con éxito.',
+    'joinSuccessfulMessage': 'Se ha unido correctamente al punto de control.',
 
-    'scanErrorMessage': 'No se pudo leer el código QR. Inténtelo de nuevo.',
+    'scanErrorMessage': 'No se pudo leer el código QR. Inténtalo de nuevo.',
 
-    'statusLoadingMessage': 'Cargando su estado...',
+    'statusLoadingMessage': 'Cargando su perfil...',
 
-    'statusNegativeMessage': 'No se han descubierto rutas de transmisión de individuos infectados a usted en este momento. Sin embargo, todos están en riesgo y las personas deben seguir las directrices de la Autoridad de Salud Pública, y el gobierno local.',
+    'statusNegativeMessage': 'En este momento no se ha descubierto ningun modo de transmisión de individuos infectados a usted.  Sin embargo, todos estamos a riesgo y individuos deben seguir las instrucciones de la Autoridad de Salud Pública y el gobierno local.',
 
-    'statusPositiveMessage': 'Se ha descubierto una posible ruta de transmisión de un individuo infectado a usted. Usted debe tomar medidas de precaución para protegerse a sí mismo y a los demás, de acuerdo con las directivas de la autoridad de salud pública y el gobierno local.',
+    'statusPositiveMessage': 'Se ha descubierto una posible vía de transmisión de una persona infectada a usted. Debe tomar medidas de precaución para protegerse y proteger a los demás, de acuerdo con las directivas de la autoridad de Salud Pública y el gobierno local.',
 
-    'loadingMessage': 'Carga...',
+    'loadingMessage': 'Cargando...',
 
     'standardRiskLevelMessage': 'normal',
 
@@ -58,15 +58,15 @@ module.exports = {
 
     'yourRiskLevelMessage': 'Su nivel de riesgo',
 
-    'aboutReportMessage': 'Si ha recibido una prueba positiva, puede descargar sus puntos de control recientes a continuación y compartir este archivo con su médico. Esto advertirá a aquellos que pueden haber estado expuestos de su mayor riesgo. Permanecerás en el anonimato.',
+    'aboutReportMessage': 'Si usted ha recibido una prueba positiva, usted puede descargar sus puntos de control recientes  y compartir su archivo con su médico. Esto advertirá a aquellos que pueden haber estado expuestos.  Usted permanecerá anónimo.',
 
-    'reportButton': 'Informe Anónimo',
+    'reportButton': 'Reporte Anónimo',
 
-    'downloadHistoryButton': 'Descargar puntos de control',
+    'downloadHistoryButton': 'Descargar los puntos de control',
 
     'aboutConfirmationCodeMessage': '¿Tiene un código de confirmación para escanear? El escaneo de un código de confirmación ayudará a aquellos que puedan haber sido expuestos, haciéndoles saber que este es un riesgo legítimo.',
 
-    'scanConfirmationCodeButton': 'Código de confirmación de escaneado',
+    'scanConfirmationCodeButton': 'Código de confirmación escaneado',
 
     'scanWithoutConfirmationCodeButton': "No tengo un código",
 
