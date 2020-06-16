efe3235b92e40fcc4f8b43b22a2d52c6e9dd332e
Tyler Yasaka <contact@tyleryasaka.me>
2020-06-15 23:05:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,22 @@ if (logToken) {
   app.use(morgan('dev', { stream: logStream }))
 }
 
+// www redirect
+if (process.env['REDIRECT_WWW'] === 'true') {
+  app.use(function (req, res, next) {
+    if (req.headers.host.match(/^www\..*/i)) {
+      // https redirect
+      if (process.env['REDIRECT_HTTPS'] === 'true') {
+        res.redirect('https://' + req.headers.host + req.url)
+      } else {
+        res.redirect('http://' + req.headers.host + req.url)
+      }
+    } else {
+      next()
+    }
+  })
+}
+
 app.use(function (req, res, next) {
   const allowOrigin = (process.env['NODE_ENV'] === 'development')
     ? req.headers.origin
@@ -37,22 +53,6 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
-
-// www redirect
-if (process.env['REDIRECT_WWW'] === 'true') {
-  app.use(function (req, res, next) {
-    if (req.headers.host.match(/^www\..*/i)) {
-      next()
-    } else {
-      // https redirect
-      if (process.env['REDIRECT_HTTPS'] === 'true') {
-        res.redirect('https://' + req.headers.host + req.url)
-      } else {
-        res.redirect('http://' + req.headers.host + req.url)
-      }
-    }
-  })
-}
 
 // https redirect
 if (process.env['REDIRECT_HTTPS'] === 'true') {
