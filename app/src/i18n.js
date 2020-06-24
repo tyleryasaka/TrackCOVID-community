ebc0cbaeb78a1787a5fbbfc6e07b358baa5a005d
Tyler Yasaka <contact@tyleryasaka.me>
2020-06-24 00:35:20 -0700

    Import translations locally

--- app/src/i18n.js
+++ app/src/i18n.js
@@ -2,39 +2,46 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import Locize from 'i18next-locize-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
+import { resources } from './translations/languages'
 
 const locizeProductId = process.env['REACT_APP_LOCIZE_PRODUCT_ID']
 
-i18n
-  .use(LanguageDetector)
-  .use(initReactI18next) // passes i18n down to react-i18next
-  .use(Locize)
-  .init({
-    backend: {
-      projectId: locizeProductId
-    },
-    load: 'all',
-    fallbackLng: 'en',
-    keySeparator: false, // we do not use keys in form messages.welcome
-    interpolation: {
-      escapeValue: false // react already safes from xss
-    },
-    detection: {
-      order: ['querystring', 'cookie', 'localStorage', 'navigator', 'htmlTag', 'path', 'subdomain'],
+const i18nSetup = locizeProductId
+  ? i18n
+    .use(LanguageDetector)
+    .use(initReactI18next) // passes i18n down to react-i18next
+    .use(Locize)
+  : i18n
+    .use(LanguageDetector)
+    .use(initReactI18next) // passes i18n down to react-i18next
 
-      // keys or params to lookup language from
-      lookupQuerystring: 'lng',
-      lookupCookie: 'i18next',
-      lookupLocalStorage: 'i18nextLng',
-      lookupFromPathIndex: 0,
-      lookupFromSubdomainIndex: 0,
+i18nSetup.init({
+  resources: locizeProductId ? undefined : resources,
+  backend: {
+    projectId: locizeProductId
+  },
+  load: 'all',
+  fallbackLng: 'en',
+  keySeparator: false, // we do not use keys in form messages.welcome
+  interpolation: {
+    escapeValue: false // react already safes from xss
+  },
+  detection: {
+    order: ['querystring', 'cookie', 'localStorage', 'navigator', 'htmlTag', 'path', 'subdomain'],
 
-      // cache user language on
-      caches: ['localStorage', 'cookie'],
+    // keys or params to lookup language from
+    lookupQuerystring: 'lng',
+    lookupCookie: 'i18next',
+    lookupLocalStorage: 'i18nextLng',
+    lookupFromPathIndex: 0,
+    lookupFromSubdomainIndex: 0,
 
-      // optional htmlTag with lang attribute, the default is:
-      htmlTag: document.documentElement
-    }
-  })
+    // cache user language on
+    caches: ['localStorage', 'cookie'],
+
+    // optional htmlTag with lang attribute, the default is:
+    htmlTag: document.documentElement
+  }
+})
 
 export default i18n
