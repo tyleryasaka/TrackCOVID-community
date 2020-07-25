913510ae4675bac9f9c5cfb13dcd55f6e2763b39
Tyler Yasaka <contact@tyleryasaka.me>
2020-07-24 21:37:53 -0700

    Support symptom onset date

--- admin/src/components/Checkpoints.js
+++ admin/src/components/Checkpoints.js
@@ -1,10 +1,15 @@
 /* globals alert, FileReader */
-import React from 'react'
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { postCheckpoints } from '../helpers/api'
 import { isValidJSON } from '../helpers/json'
 
+const oneHour = 1000 * 60 * 60
+const oneDay = oneHour * 24
+const twoDays = 2 * oneDay
+
 export function Checkpoints ({ onUpload }) {
+  const [symptomStart, setSymptomStart] = useState('')
   const { t } = useTranslation()
   const webAppUrl = process.env.REACT_APP_WEB_APP_DOMAIN
 
@@ -17,7 +22,12 @@ export function Checkpoints ({ onUpload }) {
     fr.onload = async (e) => {
       if (e && e.target.result && isValidJSON(e.target.result)) {
         const checkpointsJSON = JSON.parse(e.target.result)
-        const uploadSuccess = await postCheckpoints(checkpointsJSON)
+        const symptomStartDate = new Date(symptomStart)
+        symptomStartDate.setHours(24, 0, 0, 0)
+        const recentCheckpoints = checkpointsJSON.filter(checkpoint => {
+          return checkpoint.timestamp >= (symptomStartDate.getTime() - twoDays)
+        })
+        const uploadSuccess = await postCheckpoints(recentCheckpoints)
         if (uploadSuccess) {
           alert(t('upload_success'))
         } else {
@@ -31,18 +41,25 @@ export function Checkpoints ({ onUpload }) {
     fr.readAsText(files.item(0))
   }
 
+  const handleChangeSymptomStart = (e) => {
+    setSymptomStart(e.target.value)
+  }
+
   return (
     <div>
-      <div class='d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom'>
-        <h1 class='h2'>{t('upload_title')}</h1>
+      <div className='d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom'>
+        <h1 className='h2'>{t('upload_title')}</h1>
       </div>
       <form id='checkpoint-upload-form'>
         <p>{t('upload_instructions_1')} {webAppUrl}. {t('upload_instructions_2')}</p>
         <p>{t('upload_instructions_3')}</p>
-        <div class='alert alert-success' style={{ display: 'none' }} id='upload-success' role='alert'>
+        <div className='alert alert-success' style={{ display: 'none' }} id='upload-success' role='alert'>
           {t('upload_success')}
         </div>
-        <input class='btn btn-large btn-warning' type='file' id='checkpoint-upload' onChange={handleUpload} />
+        <span className='mr-2'>{t('upload_instructions_date')}:</span>
+        <input className='mb-2' type='date' id='start' name='symptom-start' value={symptomStart} onChange={handleChangeSymptomStart} />
+        <br />
+        <input className='btn btn-large btn-warning' type='file' id='checkpoint-upload' disabled={symptomStart === ''} onChange={handleUpload} />
       </form>
     </div>
   )
